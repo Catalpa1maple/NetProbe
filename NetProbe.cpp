e881da1ba60e749d0323e5e62901e25d081571e6
mapleMac <1155194663@link.cuhk.edu.hk>
2024-09-21 10:15:53 +0800

    getopt --testing 9/21

--- NetProbe.cpp
+++ NetProbe.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <string>
+#include <cstdlib>
 #include <cstring>
 #include <getopt.h>
 #include "TCP_socket.cpp"
@@ -12,7 +13,7 @@ enum mode {NONE,SEND,RECV,HOST};
 int main(int argc, char *argv[]) {
     int opt;
     enum mode mode = NONE;
-    int stat_interval = 500; // Default 500 ms
+    int stat = 500; // Default 500 ms
     string rhost = "localhost"; // Default localhost
     string lhost = "IN_ADDR_ANY"; // Default Late Binding
     int rport, lport= 4180; // Default 4180
@@ -24,143 +25,113 @@ int main(int argc, char *argv[]) {
     int rbufsize;
     string hostname = "localhost"; // Default localhost
 
-    static struct option long_options[] = {
-        {"send", required_argument, 0, 's'},
-        {"recv", required_argument, 0, 'r'},
-        {"host", required_argument, 0, 'h'},
-        {0, 0, 0, 0}
-    };
-    while ((opt = getopt_long(argc, argv, "s:r:h:", long_options, NULL)) != -1 && mode == NONE) {
-        switch (opt) {
-            case 's':
-                cout << "send" << endl;
-                // cout << char(opt) << endl;
-                mode = SEND;
-                break;
-            case 'r':
-                cout << "recv" << endl;
-                mode = RECV;
-                break;
-            case 'h':
-                // cout << "host" << endl;
-                mode = HOST;
-                break;
-            default:
-                cout << "Please Select Mode" << endl;
-                return EXIT_FAILURE;
-        }
-    }optind = 7;// Stop getopt()
-    cout << argv[2] << endl;
-    if (mode == SEND) {
-        static struct option send_options[] = {
-            {"stat", required_argument, 0, 't'},
-            {"rhost", required_argument, 0, 'o'},
-            {"rport", required_argument, 0, 'q'},
-            {"proto", required_argument, 0, 'p'},
-            {"pktsize", required_argument, 0, 'k'},
-            {"pktrate", required_argument, 0, 'x'},
-            {"pktnum", required_argument, 0, 'n'},
-            {"sbufsize", required_argument, 0, 'b'},
+    static struct option options[] = {
+            {"stat",     required_argument, 0, 's'},
+            {"rhost",    required_argument, 0, 'r'},
+            {"rport",    required_argument, 0, 'p'},
+            {"proto",    required_argument, 0, 'c'},
+            {"pktsize",  required_argument, 0, 'z'},
+            {"pktrate",  required_argument, 0, 'a'},
+            {"pktnum",   required_argument, 0, 'n'},
+            {"sbufsize", required_argument, 0, 'u'},
+            {"lhost",    required_argument, 0, 'l'},
+            {"lport",    required_argument, 0, 'q'},
+            {"rbufsize", required_argument, 0, 'e'},
+            {"host",     required_argument, 0, 'h'},
             {0, 0, 0, 0}
         };
 
-        while ((opt = getopt_long(argc, argv, "t:o:q:p:k:x:n:b:", send_options, NULL)) != -1) {
+    string mode_str = argv[1];//Read the mode from the command line
+    optind = 2;
+
+    if (mode_str == "-send") {
+        mode = SEND;
+        while ((opt = getopt_long(argc, argv, "s:r:p:c:z:a:n:u:", options, NULL)) != -1) {
             switch (opt) {
-                case 't':
-                    stat_interval = atoi(optarg);
-                    // cout << int(stat_interval) << endl;
+                case 's':
+                    // cout << argv[optind-1] << endl;
+                    stat = atoi(argv[optind]);
+                    cout << "stat: " << stat << endl;
                     break;
-                case 'o':
-                    rhost = optarg;
-                    break;
-                case 'q':
-                    rport = atoi(optarg);
+                case 'r':
+                    rhost = argv[optind];
+                    cout << "rhost: " << rhost << endl;
                     break;
                 case 'p':
-                    proto = optarg;
+                    rport = atoi(argv[optind]);
+                    cout << "rport: " << rport << endl;
                     break;
-                case 'k':
-                    pktsize = atoi(optarg);
+                case 'c':
+                    proto = argv[optind];
+                    cout << "proto: " << proto << endl;
                     break;
-                case 'x':
-                    pktrate = atoi(optarg);
+                case 'z':
+                    pktsize = atoi(argv[optind]);
+                    cout << "pktsize: " << pktsize << endl;
+                    break;
+                case 'a':
+                    pktrate = atoi(argv[optind]);
+                    cout << "pktrate: " << pktrate << endl;
                     break;
                 case 'n':
-                    pktnum = atoi(optarg);
+                    pktnum = atoi(argv[optind]);
+                    cout << "pktnum: " << pktnum << endl;
                     break;
-                case 'b':
-                    sbufsize = atoi(optarg);
+                case 'u':  sbufsize = atoi(argv[optind]);
+                    cout << "sbufsize: " << sbufsize << endl;
                     break;
                 default:
                     cout << "Invalid send option" << endl;
                     return EXIT_FAILURE;
             }
         }
-
-
-    } else if (mode == RECV) {
-        static struct option recv_options[] = {
-            {"stat", required_argument, 0, 't'},
-            {"lhost", required_argument, 0, 'o'},
-            {"lport", required_argument, 0, 'q'},
-            {"proto", required_argument, 0, 'p'},
-            {"pktsize", required_argument, 0, 'k'},
-            {"rbufsize", required_argument, 0, 'b'},
-            {0, 0, 0, 0}
-        };
-        while ((opt = getopt_long(argc, argv, "t:o:q:p:k:b:", recv_options, NULL)) != -1) {
-            cout << "test" << endl;
+    } else if (mode_str == "-recv") {
+        mode = RECV;
+        while ((opt = getopt_long(argc, argv, "s:l:q:c:k:e:", options, NULL)) != -1) {
             switch (opt) {
-                case 't':
-                    stat_interval = atoi(optarg);
-                    cout << "test" << endl;
+                case 's':
+                    stat = atoi(argv[optind]);
+                    cout << "stat: " << stat << endl;
                     break;
-                case 'o':
-                    lhost = optarg;
+                case 'l':
+                    lhost = argv[optind];
+                    cout << "lhost: " << lhost << endl;
                     break;
                 case 'q':
-                    lport = atoi(optarg);
+                    lport = atoi(argv[optind]);
+                    cout << "lport: " << lport << endl;
                     break;
-                case 'p':
-                    proto = optarg;
+                case 'c':
+                    proto = argv[optind];
+                    cout << "proto: " << proto << endl;
                     break;
                 case 'k':
-                    pktsize = atoi(optarg);
+                    pktsize = atoi(argv[optind]);
+                    cout << "pktsize: " << pktsize << endl;
                     break;
-                case 'b':
-                    rbufsize = atoi(optarg);
+                case 'e':
+                    rbufsize = atoi(argv[optind]);
+                    cout << "rbufsize: " << rbufsize << endl;
                     break;
                 default:
                     cout << "Invalid recv option" << endl;
                     return EXIT_FAILURE;
             }
         }
-    } else if (mode == HOST)
-    {   static struct option host_options[] = {
-            {"host", required_argument, 0, 'o'},
-            {0, 0, 0, 0}
-        };
-
-        while ((opt = getopt_long(argc, argv, "o:", host_options, NULL)) != -1) {
+    } else if (mode_str == "-host") {
+        mode = HOST;
+         while ((opt = getopt_long(argc, argv, "h:", options, NULL)) != -1) {
             switch (opt) {
-                case 'o':
-                    rhost = optarg;
+                case 'h':
+                    rhost = argv[optind];
                     break;
                 default:
                     cout << "Invalid host option" << endl;
                     return EXIT_FAILURE;
+                }
             }
-        }
-
+    } else {
+        cout << "Invalid mode" << endl;
+        return EXIT_FAILURE;
     }
-    
-
-
-
-
-
-
-
-
-
 }
